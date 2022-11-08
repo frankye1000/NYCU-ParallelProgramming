f802326c3f5ee104b99fad416df07ec6ed6bda5f
frankye100nycu <frankye100.c@nycu.edu.tw>
2022-11-08 15:01:28 +0800

    update

--- Course_Program/calculating_pi_omp_reduction.c
+++ Course_Program/calculating_pi_omp_reduction.c
@@ -4,18 +4,14 @@ static long num_steps = 10000000;
 double step; 
 
 void main() {
-    int i; double pi, sum = 0.0;
+    int i; double pi, x, sum = 0.0;
     step = 1.0/(double) num_steps;
-    #pragma omp parallel 
-    {
-        double x;
-        #pragma omp for reduction(+:sum)
+    #pragma omp parallel for private(x) reduction(+:sum)
         for (i=0;i< num_steps; i++) {
             x = (i + 0.5) * step;
             sum = sum + 4.0/(1.0 + x*x);
         }
-    }
-
+    
     pi = step * sum;
     printf("Pi = %lf\n", pi);
 }
