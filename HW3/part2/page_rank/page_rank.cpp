1e055e6a669ff23494f711aef2ef7c4d88632401
frankye100nycu <frankye100.c@nycu.edu.tw>
2022-11-13 18:49:12 +0800

    finish HW3-2-1

--- HW3/part2/page_rank/page_rank.cpp
+++ HW3/part2/page_rank/page_rank.cpp
@@ -15,45 +15,43 @@
 // damping:     page-rank algorithm's damping parameter
 // convergence: page-rank algorithm's convergence threshold
 //
+// refrence: http://jpndbs.lib.ntu.edu.tw/DB/PageRank.pdf、https://zh.wikipedia.org/wiki/PageRan
+
 void pageRank(Graph g, double *solution, double damping, double convergence)
 {
-
-  // initialize vertex weights to uniform probability. Double
-  // precision scores are used to avoid underflow for large graphs
-
   int numNodes = num_nodes(g);
   double equal_prob = 1.0 / numNodes;
-  for (int i = 0; i < numNodes; ++i)
-  {
-    solution[i] = equal_prob;
+  double *pre_solution;
+  pre_solution = (double*)malloc(sizeof(double) * numNodes);
+
+  #pragma omp parallel for
+  for (int i = 0; i < numNodes; ++i) solution[i] = equal_prob;
+
+  bool converged = false;
+  while (!converged){
+      double total_pr_diff = 0.0;
+      double no_outgoing_pr = 0.0;
+      memcpy(pre_solution, solution, numNodes * sizeof(double));
+
+      #pragma omp parallel for reduction(+:no_outgoing_pr)
+      for (int j = 0; j < numNodes; j++) no_outgoing_pr += (outgoing_size(g, j) == 0) ? damping*pre_solution[j]/numNodes : 0;
+
+      #pragma omp parallel for reduction(+:total_pr_diff)
+      for (int i = 0; i < numNodes; i++)  // ex: pr(c)
+      {  
+        const Vertex* start = incoming_begin(g, i);
+        const Vertex* end   = incoming_end(g, i);
+        double pr_i = 0.0;
+        
+        // #pragma omp parallel for reduction(+:pr_i) 這邊不加parallel比較快!!
+        // ex: pr(c) = pr(a)/2 + pr(b)/1 +...
+        for (const Vertex* v = start; v != end; v++) pr_i += pre_solution[*v] / (double)outgoing_size(g, *v);
+        pr_i = (damping * pr_i) + (1.0 - damping) / numNodes;
+        pr_i += no_outgoing_pr;
+		    solution[i] = pr_i;
+		    total_pr_diff += fabs(pr_i - pre_solution[i]);
+      }
+      converged = total_pr_diff < convergence;
   }
-
-  /*
-     For PP students: Implement the page rank algorithm here.  You
-     are expected to parallelize the algorithm using openMP.  Your
-     solution may need to allocate (and free) temporary arrays.
-
-     Basic page rank pseudocode is provided below to get you started:
-
-     // initialization: see example code above
-     score_old[vi] = 1/numNodes;
-
-     while (!converged) {
-
-       // compute score_new[vi] for all nodes vi:
-       score_new[vi] = sum over all nodes vj reachable from incoming edges
-                          { score_old[vj] / number of edges leaving vj  }
-       score_new[vi] = (damping * score_new[vi]) + (1.0-damping) / numNodes;
-
-       score_new[vi] += sum over all nodes v in graph with no outgoing edges
-                          { damping * score_old[v] / numNodes }
-
-       // compute how much per-node scores have changed
-       // quit once algorithm has converged
-
-       global_diff = sum over all nodes vi { abs(score_new[vi] - score_old[vi]) };
-       converged = (global_diff < convergence)
-     }
-
-   */
+  delete pre_solution;
 }
